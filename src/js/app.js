8d86440bb23fe35879dde3858d7388ade7ae6475
Emelie Barthelsson <emeliebarthelsson@gmail.com>
2024-11-11 23:06:15 +0100

    add questions and answers

---
+++ src/js/app.js
@@ -0,0 +1,54 @@
+const questions = [
+    {
+        question: "What is Sweden's largest island?",
+        answers: ["Gotland", "Öland", "Orust"],
+        correctAnswer: "Gotland"
+    }, 
+    {
+        question: "Which city does the Göta älv flow through?",
+        answers: ["Malmö", "Gothenburg", "Uppsala"],
+        correctAnswer: "Gothenburg"
+    }, 
+    {
+        question: "What is the deepest lake in Sweden?",
+        answers: ["Vänern", "Mälaren", "Hornavan"],
+        correctAnswer: "Hornavan"
+    }, 
+    {   question: "In which province is Kebnekaise, Sweden's highest mountain, located?",
+        answers: ["Lappland", "Härjedalen", "Jämtland"],
+        correctAnswer: "Lappland", 
+    },
+    {
+        question: "What is the largest province in Sweden by area?",
+        answers: ["Värmland", "Lappland", "Småland"],
+        correctAnswer: "Lappland"
+    }, 
+    {
+        question: "Which of these cities is located the farthest north?",
+        answers: ["Östersund", "Umeå", "Kiruna"],
+        correctAnswer: "Kiruna"
+    }, 
+    {
+        question: "Which sea borders Sweden to the east?",
+        answers: ["North Sea", "Baltic Sea", "Atlantic Ocean"],
+        correctAnswer: "Baltic sea"
+    }, 
+    {
+        question: "Which is Sweden's longest river?",
+        answers: ["Dalälven", "Klarälven", "Göta älv"],
+        correctAnswer: "Klarälven"
+    }, 
+    {
+        question: "What is the name of the largest lake located between Stockholm and Uppsala?",
+        answers: ["Vänern", "Mälaren", "Vättern"],
+        correctAnswer: "Mälaren"
+    }, 
+    {
+        question: "Which city is Sweden's third largest by population?",
+        answers: ["Uppsala", "Malmö", "Linköping"],
+        correctAnswer: "Malmö"
+    }
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
